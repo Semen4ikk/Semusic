5ea46235649e38f9ab6d1631ea9feca0f2d62f66
Semen Prokopets <semen2004vuz@yandex.ru>
2025-11-12 00:40:48 +0400

    добавил обработчик события

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,29 @@
-function App() {
-    const tracks = [
-        {id: 1, title: "Песня 1", url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3"},
-        {id: 2, title: "Песня 2", url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3"}
-    ]
+import {useState} from "react";
 
+const tracks = [
+    {id: 1, title: "Песня 1", url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3"},
+    {id: 2, title: "Песня 2", url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3"}
+]
 
-  return (
+export function App() {
+    const [selectedTrackId, setSelectedTrackId] = useState(null);
+    if ( tracks.length == 0 ) {
+        return <span>No tracks found.</span>
+    }
+
+    return (
     <>
         <h1>Music player</h1>
+        <button onClick={()=>{
+            setSelectedTrackId(null);
+        }}>Сбросить выделение</button>
         <ul>
             {tracks.map((tracks)=>{
                 return (
-                    <li key={tracks.id}>
-                        <div>
+                    <li key={tracks.id} style={{border:tracks.id === selectedTrackId ?  "1px solid yellow" : "none"                                                   }}   >
+                        <div onClick={()=> {
+                            setSelectedTrackId(tracks.id)
+                        }}>
                             {tracks.title}
                         </div>
                         <audio src={tracks.url} controls={true}>
@@ -28,4 +39,3 @@ function App() {
   )
 }
 
-export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { App } from './App.tsx'
 const rootEL = document.getElementById('root')
 const reactRoot = createRoot(rootEL!);
 reactRoot.render(<App/>)
